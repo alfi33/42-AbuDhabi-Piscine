85aba4cd13cc2efa68d4a52727fda02e294e69c9
alfi33 <althaf.33@gmail.com>
2021-07-06 10:11:58 +0400

    Add files via upload

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdul-w <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/22 01:22:25 by aabdul-w          #+#    #+#             */
+/*   Updated: 2021/05/23 16:53:25 by aabdul-w         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	first;
+	char	second;
+	char	third;
+
+	first = '0' - 1;
+	while (++first <= '9')
+	{
+		second = first;
+		while (++second <= '9')
+		{
+			third = second + 1;
+			while (third <= '9')
+			{
+				ft_putchar(first);
+				ft_putchar(second);
+				ft_putchar(third);
+				if (first != '7' || second != '8' || third != '9')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				third++;
+			}
+		}
+	}
+}
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdul-w <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/22 19:10:27 by aabdul-w          #+#    #+#             */
+/*   Updated: 2021/05/23 17:26:17 by aabdul-w         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	display(int nb)
+{
+	int	first;
+	int	second;
+
+	if (nb > 9)
+	{
+		first = nb / 10;
+		second = nb % 10;
+		ft_putchar(first + 48);
+		ft_putchar(second + 48);
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(nb + 48);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	o;
+
+	i = 0;
+	while (i <= 99)
+	{
+		o = i + 1;
+		while (o <= 99)
+		{
+			display(i);
+			ft_putchar(' ');
+			display(o);
+			if (i < 98 || o < 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			++o;
+		}
+		++i;
+	}
+}
